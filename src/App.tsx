8719375ed8168c5f52af57389de98bdbb119f669
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2025-07-25 10:25:31 +0500

    Password Generator , useref,callback,useefffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,115 +1,200 @@
-import { useState } from 'react';
+// import { useState, useCallback, useEffect, useRef } from 'react'
 
-function Square({ value, onSquareClick }) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
 
-function Board({ xIsNext, squares, onPlay }) {
-  function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
-      return;
+
+// function App() {
+//   const [length, setLength] = useState(8)
+//   const [numberAllowed, setNumberAllowed] = useState(false);
+//   const [charAllowed, setCharAllowed] = useState(false)
+//   const [password, setPassword] = useState("")
+
+//   //useRef hook
+//   const passwordRef = useRef(null)
+
+//   const passwordGenerator = useCallback(() => {
+//     let pass = ""
+//     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+//     if (numberAllowed) str += "0123456789"
+//     if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+
+//     for (let i = 1; i <= length; i++) {
+//       let char = Math.floor(Math.random() * str.length + 1)
+//       pass += str.charAt(char)
+      
+//     }
+
+//     setPassword(pass)
+
+
+//   }, [length, numberAllowed, charAllowed, setPassword])
+
+//   const copyPasswordToClipboard = useCallback(() => {
+//     passwordRef.current?.select();
+//     passwordRef.current?.setSelectionRange(0, 999);
+//     window.navigator.clipboard.writeText(password)
+//   }, [password])
+
+//   useEffect(() => {
+//     passwordGenerator()
+//   }, [length, numberAllowed, charAllowed, passwordGenerator])
+//   return (
+    
+//     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+//       <h1 className='text-white text-center my-3'>Password generator</h1>
+//     <div className="flex shadow rounded-lg overflow-hidden mb-4">
+//         <input
+//             type="text"
+//             value={password}
+//             className="outline-none w-full py-1 px-3"
+//             placeholder="Password"
+//             readOnly
+//             ref={passwordRef}
+//         />
+//         <button
+//         onClick={copyPasswordToClipboard}
+//         className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+//         >copy</button>
+        
+//     </div>
+//     <div className='flex text-sm gap-x-2'>
+//       <div className='flex items-center gap-x-1'>
+//         <input 
+//         type="range"
+//         min={6}
+//         max={100}
+//         value={length}
+//          className='cursor-pointer'
+//          onChange={(e) => {setLength(e.target.value)}}
+//           />
+//           <label>Length: {length}</label>
+//       </div>
+//       <div className="flex items-center gap-x-1">
+//       <input
+//           type="checkbox"
+//           defaultChecked={numberAllowed}
+//           id="numberInput"
+//           onChange={() => {
+//               setNumberAllowed((prev) => !prev);
+//           }}
+//       />
+//       <label htmlFor="numberInput">Numbers</label>
+//       </div>
+//       <div className="flex items-center gap-x-1">
+//           <input
+//               type="checkbox"
+//               defaultChecked={charAllowed}
+//               id="characterInput"
+//               onChange={() => {
+//                   setCharAllowed((prev) => !prev )
+//               }}
+//           />
+//           <label htmlFor="characterInput">Characters</label>
+//       </div>
+//     </div>
+// </div>
+    
+//   )
+// }
+
+// export default App
+
+import { useCallback, useState,useRef ,useEffect} from 'react';
+function MyComponent() {
+  const [length, setLength] = useState(8)
+  const [numbersAllowed, setNumbersAllowed] = useState(false)
+  const [charAllowed, setcharAllowed] = useState(false)
+  const [password, setpassword] = useState("")
+
+
+  //useRef hook
+  const passwordRef = useRef(null)
+
+
+
+  const passwordGenerator = useCallback(() => {
+    let pass = '';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    if (numbersAllowed) {
+      str += '0123456789';
     }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
+    if (charAllowed) {
+      str += '!@#$%^&*()_+[]{}|;:,.<>?';
     }
-    onPlay(nextSquares);
-  }
 
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-  }
+    for (let i = 1; i <= length; i++) {
+      let char = str.charAt(Math.floor(Math.random() * str.length) + 1);
+      pass += char;
+    }
+    setpassword(pass);
+  }, [length, numbersAllowed, charAllowed, setpassword]);
+  
+    const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+  
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numbersAllowed, charAllowed, passwordGenerator])
 
   return (
     <>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+        <h1 className='text-white text-center my-3'>Password generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            onClick={copyPasswordToClipboard}
+            className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+          >copy</button>
+
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className='cursor-pointer'
+              onChange={(e) => { setLength(e.target.value) }}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numbersAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumbersAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+              setcharAllowed((prev) => !prev)
+              }}
+            />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
+        </div>
       </div>
     </>
   );
 }
 
-export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];
-
-  function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
-  }
-
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
-  }
-
-  const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
-      description = 'Go to game start';
-    }
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
-  });
-
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-      </div>
-      <div className="game-info">
-        <ol>{moves}</ol>
-      </div>
-    </div>
-  );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+export default MyComponent;
